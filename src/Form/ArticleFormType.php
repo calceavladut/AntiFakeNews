e9ea05f4a038b2440ff310f8fedaf98147964897
andrei oprea <andrei.oprea@zitec.com>
2022-04-15 23:40:55 +0300

    final_front_andrei

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ExtractedArticle;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +12,12 @@ class ArticleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('url');
+        $builder->add('url', TextType::class, array(
+        'label' => false,
+        'attr' => array(
+            'placeholder' => 'It is FAKE?'
+        )
+    ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
